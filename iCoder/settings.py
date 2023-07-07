a7778f08c2cd6fca770e4a7c48db718e5a684ed7
Rohit <rohitjadhav.604.2@gmail.com>
2023-07-07 20:39:20 +0530

    import os

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
