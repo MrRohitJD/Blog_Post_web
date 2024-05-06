6ebb69ce28a265c05a02fde92a033c8b4e8b1d09
Rohit Jadhav <128684363+MrRohitJD@users.noreply.github.com>
2024-05-06 20:00:18 +0530

    Update settings.py static file

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR ,  "static"),
-    ]
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# if not DEBUG:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR ,  "static"),
+#     ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
